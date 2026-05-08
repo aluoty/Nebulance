edd5fdf8ce96f6253d601e7e38f9a80cbf8653af
alexanderluo666 <alexanderluo0910@gmail.com>
2026-05-08 22:02:48 +0800

    update

--- src/components/SpaceScene.tsx
+++ src/components/SpaceScene.tsx
@@ -9,25 +9,29 @@ const colors = [
   "green",
   "red",
   "yellow",
+  "pink",
+  "cyan",
 ];
 
 const planets = Array.from({ length: 12 }, (_, i) => ({
   id: i,
+
   position: [
     (Math.random() - 0.5) * 30,
     (Math.random() - 0.5) * 10,
     (Math.random() - 0.5) * 30,
   ] as [number, number, number],
+
   size: Math.random() * 1.5 + 0.5,
+
   color: colors[Math.floor(Math.random() * colors.length)],
+
   speed: Math.random() * 0.01 + 0.001,
 }));
 
-
-
 export default function SpaceScene() {
   return (
-    <Canvas camera={{ position: [0, 5, 12], fov: 75 }}>
+    <Canvas camera={{ position: [0, 5, 15], fov: 75 }}>
       <ambientLight intensity={1.5} />
 
       <pointLight position={[10, 10, 10]} intensity={2} />
@@ -41,16 +45,17 @@ export default function SpaceScene() {
         fade
         speed={1}
       />
+
       {planets.map((planet) => (
-  	<Planet
-    	  key={planet.id}
+        <Planet
+          key={planet.id}
           position={planet.position}
           size={planet.size}
           color={planet.color}
           speed={planet.speed}
         />
       ))}
-     
+
       <OrbitControls />
     </Canvas>
   );
